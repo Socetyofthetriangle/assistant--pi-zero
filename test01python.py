e2a05f4907368ffc1938734619dfa265fe7e624d
Wylgrif <164638876+Wylgrif@users.noreply.github.com>
2024-05-28 20:20:38 +0200

    Update test01python.py

--- test01python.py
+++ test01python.py
@@ -6,12 +6,20 @@ tts = RealtimeTTS()
 r = sr.Recognizer()
 client = OpenAI(base_url="http://192.168.1.121:1234/v1", api_key="lm-studio")
 
-completion = client.chat.completions.create(
-  model="TheBloke/Mistral-7B-Instruct-v0.2-GGUF",
-  messages=[
-    {"role": "user", "content": prompt+"Réponse en Français souhaitée."}
-  ],
-  temperature=0.7,
-)
 
-#print(completion.choices[0].message.content)
+while True:
+  with sr.Microphone() as source:
+    print("Parlez maintenant...")
+    audio = r.listen(source)
+    print('fin de l'enregistrement')
+    prompt=r.recognize_google(audio, language='fr-FR')
+  completion = client.chat.completions.create(
+    model="TheBloke/Mistral-7B-Instruct-v0.2-GGUF",
+    messages=[
+      {"role": "user", "content": prompt+"Réponse en Français souhaitée."}
+    ],
+    temperature=0.7,
+  )
+  txts=completion.choices[0].message.content
+  print(txts)
+  tts.speak(txts)
